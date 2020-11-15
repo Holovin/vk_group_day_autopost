c65ba66aa83f5940bc7e1181f73e16d769c446f2
Alexander Holovin <dvjmixxxer@gmail.com>
2020-11-15 05:46:37 +0300

    pm2 config

---
+++ ecosystem.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+    apps : [{
+        name: 'vk_group_day_autopost',
+
+        script: 'npm',
+        args: 'go',
+
+        instances: 1,
+        autorestart: true,
+        watch: false,
+        max_memory_restart: '1G',
+
+        env: {
+            NODE_ENV: 'development',
+        },
+        env_production: {
+            NODE_ENV: 'production'
+        }
+    }],
+};
